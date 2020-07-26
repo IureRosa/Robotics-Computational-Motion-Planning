495fe66db8b013d6c695277664c57eef124a49f2
Iure Rosa <63236473+IureRosa@users.noreply.github.com>
2020-07-25 22:54:52 -0300

    Initial commit

---
+++ Assignment2_CSpace/triangle_intersection.m
@@ -0,0 +1,10 @@
+function flag = triangle_intersection(P1, P2)
+% triangle_test : returns true if the triangles overlap and false otherwise
+
+%%% All of your code should be between the two lines of stars.
+% *******************************************************************
+P1 = [[2, 3]; [1,0]; [3,0]];
+P2 = [[3,4]; [2,1]; [4,1]];
+flag = ~true;
+% *******************************************************************
+end
